ba357ca18fb52437db93193f5d28a854636b8229
SYEDAFFAN2003 <SYEDAFFAN7387@GMAIL.COM>
2024-11-03 18:26:29 +0530

    Delete hist_eq.py

--- hist_eq.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-img = cv2.imread('C:/Users/syeda/Downloads/th.jpeg', 0) 
-equalized = cv2.equalizeHist(img)
-
-cv2.imshow('Equalized Image', equalized)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
